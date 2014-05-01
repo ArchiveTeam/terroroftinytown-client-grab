fb15ef656951cab8c5c9b86147768f4a93415ac2
Christopher Foo <chris.foo@gmail.com>
2014-04-30 21:30:08 -0400

    WIP, adds some print statements.

--- scraper.py
+++ scraper.py
@@ -15,10 +15,11 @@ def main():
     tracker_client = TrackerClient(
         tracker_host,
         username,
-        client_version=client_version,
+        version=client_version,
         bind_address=bind_address
     )
 
+    print('Getting item from tracker.')
     item_info = try_with_tracker(tracker_client.get_item)
 
     scraper_client = Scraper(
@@ -34,9 +35,11 @@ def try_with_tracker(func, *args, **kwargs):
     for try_count in itertools.count():
         try:
             return func(*args, **kwargs)
-        except TrackerError:
-            # TODO: logging
-            time.sleep(60 * try_count)
+        except TrackerError as error:
+            sleep_time = 60 * try_count
+            print('Error communicating with tracker: {0}.'.format(error))
+            print('Trying again in {0} seconds.'.format(sleep_time))
+            time.sleep(sleep_time)
 
             if try_count > 10:
                 raise
